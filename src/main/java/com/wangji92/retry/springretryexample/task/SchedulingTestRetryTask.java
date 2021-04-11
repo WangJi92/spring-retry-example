1b9d45e3678687f1b4b93bb5d8f8247b275417a0
wangji <983433479@qq.com>
2021-04-11 21:16:35 +0800

    增加 guava retry 多线程问题

--- src/main/java/com/wangji92/retry/springretryexample/task/SchedulingTestRetryTask.java
+++ src/main/java/com/wangji92/retry/springretryexample/task/SchedulingTestRetryTask.java
@@ -1,9 +1,9 @@
 package com.wangji92.retry.springretryexample.task;
 
-import com.github.rholder.retry.Retryer;
-import com.github.rholder.retry.RetryerBuilder;
-import com.github.rholder.retry.StopStrategies;
-import com.github.rholder.retry.WaitStrategies;
+import com.github.rholder.retry.*;
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.SimpleTimeLimiter;
+import com.google.common.util.concurrent.TimeLimiter;
 import com.wangji92.retry.springretryexample.service.RetryTestService;
 import com.wangji92.retry.springretryexample.utils.AopTargetUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +21,10 @@ import org.springframework.retry.support.RetryTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Nonnull;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -128,6 +132,83 @@ public class SchedulingTestRetryTask {
             log.info("guava retry retryTestService response result is {}", responseBody);
 
 
+        }
+    }
+
+    /**
+     * guava withAttemptTimeLimiter 这里如果多线程支持 会导致很多的问题 上下文传递、而且框架没有人升级了
+     */
+    @ConditionalOnExpression("#{'true'.equals(environment['programmingGuavaRetryLimitTime'])}")
+    @Configuration
+    public class ProgrammingGuavaRetryLimitTime {
+        @Scheduled(fixedRate = 30000)
+        public void programmingGuavaRetry() throws Exception {
+            // 获取原始的对象
+            RetryTestService targetRetryTestService = (RetryTestService) AopTargetUtils.getTarget(retryTestService);
+
+            // RetryerBuilder 构建重试实例 guavaRetryer,可以设置重试源且可以支持多个重试源，可以配置重试次数或重试超时时间，以及可以配置等待时间间隔
+            Retryer<Integer> guavaRetryer = RetryerBuilder.<Integer>newBuilder()
+                    //设置异常重试源 根据异常 也可以 retryIfResult 根据结果
+                    .retryIfExceptionOfType(RemoteAccessException.class)
+
+                    // 【这里将会使用多线程执行(other 线程执行、会导致事务问题、线程上下文传递问题 一定要小心)】 还有这个框架 这个属性高版本不支持了.
+                    .withAttemptTimeLimiter(new FixedAttemptTimeLimit<Integer>(1, TimeUnit.MINUTES))
+                    //设置等待间隔时间
+                    .withWaitStrategy(WaitStrategies.fixedWait(5, TimeUnit.SECONDS))
+                    //设置最大重试次数
+                    .withStopStrategy(StopStrategies.stopAfterAttempt(2))
+                    .build();
+            Integer responseBody = null;
+            try {
+                log.info("guava retry retryTestService begin thread name={}", Thread.currentThread().getName());
+                responseBody = guavaRetryer.call(() -> {
+                    log.info("current thread name={}", Thread.currentThread().getName());
+                    return targetRetryTestService.retryTestService();
+                });
+                log.info("guava retry retryTestService response result is {}", responseBody);
+            } catch (Exception e) {
+                log.info("guava retry error", e);
+                responseBody = targetRetryTestService.recover(null);
+            }
+
+            log.info("guava retry retryTestService response result is {}", responseBody);
+
+
+        }
+
+
+        /**
+         * {@literal https://github.com/rholder/guava-retrying/issues/66 这个框架没有人支持升级}
+         * guava retry 这里过期了..
+         *
+         * @param <V>
+         */
+        private final class FixedAttemptTimeLimit<V> implements AttemptTimeLimiter<V> {
+
+            private final TimeLimiter timeLimiter;
+            private final long duration;
+            private final TimeUnit timeUnit;
+
+            public FixedAttemptTimeLimit(long duration, @Nonnull TimeUnit timeUnit) {
+                this(SimpleTimeLimiter.create(Executors.newFixedThreadPool(10)), duration, timeUnit);
+            }
+
+            public FixedAttemptTimeLimit(long duration, @Nonnull TimeUnit timeUnit, @Nonnull ExecutorService executorService) {
+                this(SimpleTimeLimiter.create(executorService), duration, timeUnit);
+            }
+
+            private FixedAttemptTimeLimit(@Nonnull TimeLimiter timeLimiter, long duration, @Nonnull TimeUnit timeUnit) {
+                Preconditions.checkNotNull(timeLimiter);
+                Preconditions.checkNotNull(timeUnit);
+                this.timeLimiter = timeLimiter;
+                this.duration = duration;
+                this.timeUnit = timeUnit;
+            }
+
+            @Override
+            public V call(Callable<V> callable) throws Exception {
+                return timeLimiter.callWithTimeout(callable, duration, timeUnit);
+            }
         }
     }
 
